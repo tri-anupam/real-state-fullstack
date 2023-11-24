d341479df6752a2e434209603af5af0e61358eee
Anupam Tripathi <arvindtri.70@gmail.com>
2023-11-24 14:45:19 +0530

    changes in file

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
       <div className="flexCenter innerWidth paddings h-container">
         {/* logo */}
         <Link to="/">
-          <img src="./logo.png" alt="logo" width={100} />
+          <h2>LUXELOCALES</h2>
         </Link>
 
         {/* menu */}
